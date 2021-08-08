9df0ccad3abe50e7850c6a3640ebaff831c5df27
Abhinav Mishra <86315672+abhinav1590@users.noreply.github.com>
2021-08-08 16:08:57 +0530

    main.py
    eye detection added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
-import cv2 as cv
 
 face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
 
 cap = cv2.VideoCapture(0)
 
@@ -10,17 +10,22 @@ while True:
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    faces = face.detectMultiScale(gray, 1.1, 4)
+    faces = face.detectMultiScale(gray, 1.1, 3)
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.rectangle(img, (x, y), (x+w, y+h) ,(255, 0, 0), 3)
+
+        r1= gray[y:y+h , x:x+w]
+        r2= img[ y:y+h , x:x+w]
+
+        eyes=eye.detectMultiScale(r1)
+
+        for(ex,ey,ew,eh) in eyes:
+            cv2.rectangle(r2, (ex, ey), (ex+ew, ex+eh), (255, 0, 255), 2)
 
         cv2.imshow('img', img)
 
-        k = cv2.waitKey(30) & 0xff
+        k = cv2.waitKey(1) & 0xff
         if k == 27:
             break
-
 cap.release()
-
-
